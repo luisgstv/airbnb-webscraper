46344be5222d062688c76650c029190367ac76c9
Luis Gustavo <luisgustavodamas2006@outlook.com>
2024-04-26 21:27:54 -0300

    Add comments
    Added comments throughout the script for easier understanding.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from math import floor
 
 class Scraper:
     def __init__(self):
-        # Criando as opções
+        # Creating the ChromeOptions
         self.options = webdriver.ChromeOptions()
         self.options.add_argument('window-size=800,600')
         self.options.add_argument('--lang=en-US')
@@ -21,6 +21,7 @@ class Scraper:
         start = time.perf_counter()
         self.go_to_scrape_page(self.place, self.page_number)
 
+        # Scraping the pages until it reaches the end
         self.page = 1
         self.data = []
         while True:
@@ -36,6 +37,7 @@ class Scraper:
         print(f'Took {stop - start} seconds to scrape.')
 
     def ask_info(self):
+        # Asking information about the search and how many pages will be scraped
         self.place = input('Where do you wanna go? ')
         self.page_number = input('How many pages you wanna scrape? \n(1) The first page\n(2) 50% of the pages\n(3) 100% of the pages\n')
         if self.page_number not in ('1', '2', '3'):
@@ -43,29 +45,31 @@ class Scraper:
             self.ask_info()
 
     def go_to_scrape_page(self, place, page_number):
-        # Inicializando o driver
+        # Creating the webdriver
         self.driver = webdriver.Chrome(options=self.options)
 
-        # Indo para a página e aguardando carregar
+        # Going to the page and waiting for it to load
         self.driver.get('https://www.airbnb.com')
 
         WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="site-content"]//img'))
         )
 
-        # Digitando no campo de localização onde quero ir
+        # Writing the place in the search field
         self.driver.find_element(By.ID, 'bigsearch-query-location-input').send_keys(place)
         
-        # Realizando a pesquisa e aguardando a pagina carregar
+        # Searching and waiting for the page to load
         self.driver.find_element(By.XPATH, '//button[@data-testid="structured-search-input-search-button"]').click()
 
         WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="site-content"]//nav'))
         )
 
+        # Checking if the cookies banner appeared
         if len(self.driver.find_elements(By.XPATH, '//div[@data-testid="main-cookies-banner-container"]')) == 1:
             self.driver.find_elements(By.XPATH, '//div[@data-testid="main-cookies-banner-container"]/section//button')[-1].click()
 
+        # Setting the number of pages to scrape
         if page_number == '1':
             self.pages = 1
         elif page_number == '2':
@@ -75,20 +79,20 @@ class Scraper:
             self.pages = int(self.driver.find_elements(By.XPATH, '//*[@id="site-content"]//nav//a')[-2].text)
 
     def scrape(self):
-        # Pegar o html content dessa página para trabalhar com bs4
+        # Parsing the page's HTML
         self.soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 
-        # Pegando as divs com as estadias
+        # Getting the divs with the stays
         self.stays = self.soup.find_all('div', attrs={'itemprop': 'itemListElement'})
         for stay in self.stays:
-            # Pegando as informações
+            # Getting the data
             title = stay.find('div', attrs={'data-testid': 'listing-card-title'})
             *house_type, description, _ = stay.find_all('div', attrs={'data-testid': 'listing-card-subtitle'})
             price = stay.find('div', attrs={'data-testid': 'price-availability-row'})
             avaliation = self.driver.find_element(By.XPATH, '//*[@itemprop="itemListElement"]//div/span/span[@aria-hidden="true"]')
             url = stay.find('meta', attrs={'itemprop': 'url'})
 
-            # Limpando as informações
+            # Cleaning the data
             title = title.text
 
             house_type = house_type[0].find_all('span')
@@ -108,9 +112,11 @@ class Scraper:
 
             url = url['content']
 
+            # Appending the data
             self.data.append([title, house_type, description, price, avaliation, url])
 
     def next_page(self):
+        # Going to the next page and waiting for it to load all the cards
         self.driver.find_elements(By.XPATH, '//*[@id="site-content"]//nav//a')[-1].click()
 
         while len(self.driver.find_elements(By.XPATH, '//*[@id="site-content"]//*[@data-testid="listing-card-title"]')) < len(self.stays):
@@ -119,11 +125,12 @@ class Scraper:
         self.page += 1
 
     def save_data_frame(self):
-        # Gerando arquivo csv
+        # Generating the data frame and saving it in CSV and Excel formats
         data_frame = pd.DataFrame(self.data, columns=['Title', 'Type', 'Description', 'Price', 'Avaliation', 'URL'])
         data_frame.to_csv(f'{self.place}.csv', index=False)
         data_frame.to_excel(f'{self.place}.xlsx', index=False)
 
+# Running the application
 if __name__ == '__main__':  
     webscraper = Scraper()
     webscraper.run()
